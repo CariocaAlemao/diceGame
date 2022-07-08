fbd003f064063f29b0b77eb0b916b9d5ceeacde5
Gabriel Fernandes <107198263+CariocaAlemao@users.noreply.github.com>
2022-07-08 11:31:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+alert("Hello, this is a little dice game... play as much as you like :)")
+
+
+document.querySelector(".start").addEventListener("click", startDice);
+
+
+function startDice(){
+    
+    var randomNumber1 = Math.floor(Math.random()*6)+1;
+    document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+    
+    var randomNumber2 = Math.floor(Math.random()*6)+1;
+    document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+    
+    if(randomNumber1<randomNumber2){
+        document.querySelector("h1").innerHTML="Player 2 Wins!";
+    }else if(randomNumber2<randomNumber1){
+        document.querySelector("h1").innerHTML="Player 1 Wins!";
+    }else{
+        document.querySelector("h1").innerHTML="Draw!";
+    }
+}
+
